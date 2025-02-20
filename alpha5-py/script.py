02e70da20f0ebe99c1d90bcaa4e3093363f2f309
Mitch Taylor <mitchell.taylor@inovexcorp.com>
2025-02-20 10:24:21 -0500

    read file and print to csv

--- alpha5-py/script.py
+++ alpha5-py/script.py
@@ -11,16 +11,19 @@ data ="""<page>
  <content>Welcome to Chapter 2</content>
 </page>"""
 
-soup = BeautifulSoup(data, "html.parser")
+with open('DISA_STIG_Red_Hat_Enterprise_Linux_9_v2r2.audit', "r") as xml_file:
+    stig = xml_file.read()
+
+soup = BeautifulSoup(stig, "html.parser")
 
 ########### Title #############
-required0 = soup.find_all("title")
+required0 = soup.find_all("item")
 title = []
 for i in required0:
     title.append(i.get_text())
 
 ########### Content #############
-required0 = soup.find_all("content")
+required0 = soup.find_all("custom_item")
 content = []
 for i in required0:
     content.append(i.get_text())
@@ -28,3 +31,9 @@ for i in required0:
 doc1 = list(zip(title, content))
 for i in doc1:
     print(i)
+
+filename = 'example2.csv'
+
+with open(filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(doc1)
