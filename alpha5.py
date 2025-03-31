0711ec741ad851429536e1976af96e6bbdd80907
Mitch Taylor <mitchell.taylor@inovexcorp.com>
2025-03-30 21:13:20 -0400

    speed improvement

--- alpha5.py
+++ alpha5.py
@@ -3,14 +3,12 @@ import sys
 import csv
 import xml.etree.ElementTree as ET
 
-
 # maybe make some logic to enforce that the first arg is a .audit and the second is a .xml for this `if`
 if len(sys.argv) > 1:
     auditArg = sys.argv[1]
     manualArg = sys.argv[2]
 else:
-    # change this help message to reflect the final name of the script (which probably won't be `script5` lol)
-    print("Please run the script with this format: python3 script5.py path/to/audit/file.audit path/to/manual/file.xml")
+    print("Please run the script with this format: python3 alpha5.py path/to/audit/file.audit path/to/manual/file.xml")
 
 
 
@@ -61,16 +59,29 @@ manualData = extract_group_data(manualArg)
 
 def fetch_nested_item_content(xml_data):
     item_pattern = r'<(custom_item|item)>(.*?)</\1>'
-    
+    eight_pattern = r'(?<=800-53\|)(.*?)(?=,)'
+    cat_pattern = r'(?<=CAT\|)(.*?)(?=,)'
+    stig_pattern = r'(?<=STIG-ID\|)(.*?)(?=,)'
+
+    # recursively fetch content    
     def extract_items(data):
         items = re.findall(item_pattern, data, re.DOTALL)
 
-        # recursively extract content
+        if not items:
+            return []
+
         nested_items = []
         for tag, content in items:
-            nested_items.append(content.strip())
+            stig1 = re.search(stig_pattern, content.strip())
+            cat1 = re.search(cat_pattern, content.strip())
+            eight1 = re.search(eight_pattern, content.strip())
+            item_dict = {
+                'STIG': stig1.group(1) if stig1 else None,
+                'CAT': cat1.group(1) if cat1 else None,
+                '800-53': eight1.group(1) if eight1 else None
+            }
+            nested_items.append(item_dict)
             nested_items.extend(extract_items(content))
-
         return nested_items
     return extract_items(xml_data)
 
@@ -84,19 +95,13 @@ items = fetch_nested_item_content(stig)
 
 # populate array that will be turned into the csv file
 finalArr = []
-eight_pattern = r'(?<=800-53\|)(.*?)(?=,)'
-cat_pattern = r'(?<=CAT\|)(.*?)(?=,)'
-stig_pattern = r'(?<=STIG-ID\|)(.*?)(?=,)'
 for manualEntry in manualData:
     for auditEntry in items:
-        tempVar = re.search(stig_pattern, auditEntry).group(1) if re.search(stig_pattern, auditEntry) else None
-        if tempVar == manualEntry['Version (Stig-ID)']:
+        if auditEntry['STIG'] == manualEntry['Version (Stig-ID)']:
             # Add new keys directly to the manualEntry dictionary
-            manualEntry['CAT'] = re.search(cat_pattern, auditEntry).group(1) if re.search(cat_pattern, auditEntry) else None
-            manualEntry['800-53'] = re.search(eight_pattern, auditEntry).group(1) if re.search(eight_pattern, auditEntry) else None
+            manualEntry['CAT'] = auditEntry['CAT']
+            manualEntry['800-53'] = auditEntry['CAT']
             finalArr.append(manualEntry)
-
-
 
 # then convert finalArr into the csv file
 csv_file = 'output.csv'
